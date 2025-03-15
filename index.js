2fe0c8ee0c8907af235a8ff8d9d0f2d7b56b841d
Yvonne-del <ys359375@gmail.com>
2025-03-15 21:13:09 +0300

    map array iteration

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((tutorial) => {
+    return tutorial
+      .split(" ") // Split the sentence into words
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize each word
+      .join(" "); // Join words back into a sentence
+})}
+console.log(titleCased()); // To test output
